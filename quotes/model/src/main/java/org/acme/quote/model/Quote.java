66a6976dd148bc1fa0ed9e02f857d4c767226d1d
Adler Fleurant <2609856+AdlerFleurant@users.noreply.github.com>
2023-03-29 17:47:25 -0400

    Ensure that Object is registered for reflections

--- quotes/model/src/main/java/org/acme/quote/model/Quote.java
+++ quotes/model/src/main/java/org/acme/quote/model/Quote.java
@@ -21,22 +21,6 @@ public class Quote implements Serializable {
         this.price = price;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
     @Override
     public String toString() {
         return "Quote{" +
